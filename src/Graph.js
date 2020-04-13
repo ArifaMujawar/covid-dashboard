b07dbdaac927115a309a446c58de119d32b24ca4
ArifaMujawar <arifabegam.mujawar@gmail.com>
2020-04-13 17:04:59 +0300

    graphs responsive

--- src/Graph.js
+++ src/Graph.js
@@ -1,30 +1,42 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent } from "react";
 import {
-  AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip
-} from 'recharts';
-
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  ResponsiveContainer,
+  Tooltip,
+} from "recharts";
 
 export default class Graph extends PureComponent {
-   
   render() {
     return (
-      <AreaChart
-        
-        width={500}
-        height={400}
-        data={this.props.weekData}
-        margin={{
-          top: 10, right: 30, left: 0, bottom: 0,
-        }}
-      >
-      
-        <CartesianGrid strokeDasharray="3 3" />
-        <XAxis dataKey={this.props.XAxisDatakey}/>
-        <YAxis />
-        <Tooltip />
-        
-        <Area type="monotone" dataKey={this.props.AreaDataKey} stroke="#2768e9" fill={this.props.fill} />
-      </AreaChart>
+      <div style={{ width: 350, height: 300 }}>
+        <ResponsiveContainer>
+          <AreaChart
+            data={this.props.weekData}
+            margin={{
+              top: 10,
+              right: 30,
+              left: 0,
+              bottom: 0,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey={this.props.XAxisDatakey} />
+            <YAxis />
+            <Tooltip />
+
+            <Area
+              type="monotone"
+              dataKey={this.props.AreaDataKey}
+              stroke="#2768e9"
+              fill={this.props.fill}
+            />
+          </AreaChart>
+        </ResponsiveContainer>
+      </div>
     );
   }
 }
